938859ea3a71ab7c45ef6348f4cb1848a0c51608
Katie So <katieso86@gmail.com>
2019-10-09 17:34:28 -0700

    can handle calculations with 2 values and 1 operator, hahave some bugs to fix

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,81 @@
-console.log("hello world");
+document.addEventListener("DOMContentLoaded", function(){
+  console.log("js loaded");
+});
 
-$(document).ready(function(){
-  var word = "letter";
+  var allDigits = []
+  var total = 0
+  var equation = []
+  var letter = document.getElementsByClassName("digit");
+  var operator = document.getElementsByClassName("operator");
 
-  for(i=0; i<word.length; i++){
-    $(".numChances").append("<b class='" + word[i] + "'> _ </b> " );
+  //on click, push this.value into an array
+  for(var i = 0; i< letter.length; i++){
+    letter[i].addEventListener('click', function(e){
+        allDigits.push(e.target.value);
+        console.log("equation: "+ equation)
+        console.log("alldigits: "+ allDigits)
+  //join the ele from the arr to form string and append on UI
+        if (total === 0){
+        document.getElementById("total").innerHTML = allDigits.join("")
+      } else {
+        document.getElementById("total").innerHTML = total + allDigits.join("")
+
+      }
+    })
   };
 
-  var alpahabet = "abcdefghijklmopqrstuvwxyz";
+function getTotal(){
+  // combine the 2nd set of digits
+  equation.push(allDigits.join(""))
+  clearArr()
 
+  //determine which operator was used
+  for (var i = 0; i< equation.length; i++){
+    if (equation[1]==="+"){
+      total = parseInt(equation[0]) + parseInt(equation[2]);
+      showTotal()
+    } if (equation[1]==="-"){
+      total = parseInt(equation[0]) - parseInt(equation[2]);
+      showTotal()
+    } if (equation[1]==="*"){
+      total = parseInt(equation[0]) * parseInt(equation[2]);
+      showTotal()
+    } if (equation[1]==="/"){
+      total = parseInt(equation[0]) / parseInt(equation[2]);
+      showTotal()
+    }
+  }
+}
 
-//append num of letters
-  for(j=0; j<alpahabet.length; j++){
-    $(".alpahabet").append("<button class='" + alpahabet[j] + "'>" + alpahabet[j] + " </button>");
+  function showTotal(){
+    document.getElementById("total").innerHTML = total
+    document.getElementById("op").innerHTML = " "
+    equation = []
+    allDigits = [];
   }
 
-//restart button
-  var resetGame = $( ".tryAgain" ).on( "click", function() {
-    location.reload()
-  })
-
-//on click check if the selected letter match
-  var count = 6
-  $( ".alpahabet button" ).on( "click", function() {
-    var pressButton = $(this).attr('class');
-    if($(".numChances b").hasClass(pressButton) && count > 0){
-      $("."+pressButton).html(pressButton);
-      console.log(count)
-    } if($(".numChances b").hasClass(pressButton)=== false){
-      count --;
-      console.log("not a match " +count)
-    } if(count === 0){
-      console.log("end of game")
-      $(".alpahabet").attr('id', 'hide');
-      $(".gameover").attr('id', 'show')
-      $(".tryAgain").attr('id', 'show')
-    }
-  });
+  function clearArr(){
+    allDigits = [];
+  }
 
 
-});
+  for(var i = 0; i< operator.length; i++){
+    operator[i].addEventListener('click', function(e){
+      //Do nothing if there is no number before a operator
+      if (allDigits[0] === operator[i]){
+        console.log("do nothing")
+      } if (equation.length === 2){
+            getTotal();
+            console.log("run get total")
+          } else {
+      //push the combined digits into an arr, followed by the operator
+        allDigits.unshift(total);
+        equation.push(allDigits.join(""))
+        equation.push(e.target.value)
+    //clear the digits arr since we have already combined the digis
+        clearArr()
+    //show which operator selected
+        document.getElementById("op").innerHTML = e.target.value
+      }
+    })
+  };
